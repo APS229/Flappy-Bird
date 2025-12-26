fde42ab4f7d598f7142b56afcd4abc5a95e4aa6f
APS229 <apsofficial229@gmail.com>
2025-12-26 18:54:03 +0530

    Add 2D support & generate pillars dynamically

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 100);
@@ -8,46 +10,50 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const minYaw = -Math.PI / 4;
-const maxYaw = Math.PI / 4;
-const rotationSpeed = 0.002; // radians
+const minView = -Math.PI / 4;
+const maxView = Math.PI / 4;
+const viewSpeed = 0.002; // radians
 let yaw = 0;
 let pitch = 0;
+let twoDimensional = false;
 camera.rotation.order = 'YXZ';
 
-const pillarWidth = 5;
-const pillarHeight = 100;
-const pillarDepth = 5;
+const PILLAR_WIDTH = 5;
+const PILLAR_HEIGHT = 100;
+const PILLAR_DEPTH = 5;
+const PILLAR_SPEED = 0.2;
+const PILLAR_GAP_SIZE = 110;
+const PILLAR_GAP_POSITION = 24;
+const pillarGeometry = new THREE.BoxGeometry(PILLAR_WIDTH, PILLAR_HEIGHT, PILLAR_DEPTH);
 
-const lastPositionZ = -100;
 
 const pillars = [];
-const totalPillars = 5;
+const totalPillars = 8;
+const lastPositionZ = -(totalPillars * 20);
 
-const colors = ['blue', 'yellow', 'red', 'green', 'pink'];
+const PLAYER_DIMENSIONS = 3;
+const PLAYER_SPEED = 0.2;
+const playerGeometry = new THREE.BoxGeometry(PLAYER_DIMENSIONS, PLAYER_DIMENSIONS, PLAYER_DIMENSIONS);
 
-const pillarGeometry = new THREE.BoxGeometry(pillarWidth, pillarHeight, pillarDepth);
-
-const playerGeometry = new THREE.BoxGeometry(3, 3, 3);
 let player = null;
 
 class Pillar {
     constructor(id, positionZ) {
         this.id = id;
-        this.material = new THREE.MeshPhongMaterial({ color: colors[id] });
+        this.material = new THREE.MeshPhongMaterial({ color: 'green' });
         this.top = new THREE.Mesh(pillarGeometry, this.material);
         this.bottom = new THREE.Mesh(pillarGeometry, this.material);
         this.top.position.z = positionZ;
         this.bottom.position.z = positionZ;
         this.z = positionZ;
-        this.setRandomGap();
         this.canMove = false;
+        this.setRandomGapPosition();
     }
 
     move() {
-        this.top.position.z += 0.1;
-        this.bottom.position.z += 0.1;
-        this.z += 0.1;
+        this.z += PILLAR_SPEED;
+        this.top.position.z = this.z;
+        this.bottom.position.z = this.z;
     }
 
     resetPosition(z) {
@@ -56,38 +62,36 @@ class Pillar {
         this.z = z;
     }
 
-    setRandomGap() {
-        this.gap = parseInt((Math.random() * 16) + 1);
-        // -45 to 61 is bottom limit
-        this.bottom.position.y = -45 - this.gap;
-        this.top.position.y = 61 - this.gap;
+    setRandomGapPosition() {
+        this.randomGapPosition = parseInt((Math.random() * PILLAR_GAP_POSITION) + 1);
+        this.top.position.y = (PILLAR_GAP_SIZE / 2) + this.randomGapPosition / 2;
+        this.bottom.position.y = -(PILLAR_GAP_SIZE / 2) + this.randomGapPosition / 2;
     }
 }
 
 class Player {
     constructor() {
-        this.material = new THREE.MeshPhongMaterial({ color: 'orange' });
+        this.material = new THREE.MeshPhongMaterial({ color: 'yellow' });
         this.mesh = new THREE.Mesh(playerGeometry, this.material);
         this.canMove = false;
     }
 
     moveUp() {
         if (this.mesh.position.y > 23) return;
-        camera.position.y += 0.1;
-        player.mesh.position.y += 0.1;
+        if (!twoDimensional) camera.position.y += PLAYER_SPEED;
+        player.mesh.position.y += PLAYER_SPEED;
     }
 
     moveDown() {
         if (this.mesh.position.y < -23) return;
-        camera.position.y -= 0.1;
-        player.mesh.position.y -= 0.1;
+        if (!twoDimensional) camera.position.y -= PLAYER_SPEED;
+        player.mesh.position.y -= PLAYER_SPEED;
     }
 }
 
 init();
 function init() {
     scene.background = new THREE.Color('grey');
-    scene.fog = new THREE.Fog('white', 30, 100);
 
     player = new Player();
     scene.add(player.mesh);
@@ -101,8 +105,13 @@ function init() {
         scene.add(pillar.bottom);
     }
 
-    const light = new THREE.AmbientLight('white', 3);
-    scene.add(light);
+    const dLight = new THREE.DirectionalLight('white', 1);
+    const aLight = new THREE.AmbientLight('white', 2);
+    scene.add(dLight);
+    scene.add(aLight);
+
+    // Camera perspective from front of the bird
+    camera.position.z = -PLAYER_DIMENSIONS / 2;
 }
 
 function animate() {
@@ -115,15 +124,18 @@ function animate() {
         if (pillar.canMove) pillar.move();
 
         // Collision detection
-        if ((pillar.top.position.y - pillarHeight / 2 <= player.mesh.position.y + 3 / 2 || pillar.bottom.position.y + pillarHeight / 2 >= player.mesh.position.y - 3 / 2) &&
-            pillar.z + pillarDepth / 2 >= player.mesh.position.z - 3 / 2) {
-            console.log("collision with pillar");
+        if ((pillar.top.position.y - PILLAR_HEIGHT / 2 <= player.mesh.position.y + PLAYER_DIMENSIONS / 2 ||
+            pillar.bottom.position.y + PILLAR_HEIGHT / 2 >= player.mesh.position.y - PLAYER_DIMENSIONS / 2) &&
+            pillar.z + PILLAR_DEPTH / 2 >= player.mesh.position.z - PLAYER_DIMENSIONS / 2 &&
+            pillar.z - PILLAR_DEPTH / 2 <= player.mesh.position.z + PLAYER_DIMENSIONS / 2) {
+            console.log('dead');
+            player.material.color.setColorName('red');
         }
 
-        // Pillars leave the view of camera
-        if (pillar.z >= 0) {
-            pillar.resetPosition(lastPositionZ);
-            pillar.setRandomGap();
+        // Pillar goes behind the bird
+        if (pillar.z >= 60) {
+            pillar.resetPosition(lastPositionZ + 60);
+            pillar.setRandomGapPosition();
         }
     }
     renderer.render(scene, camera);
@@ -132,6 +144,12 @@ function animate() {
 let keyPressed = false;
 function onKeyDown(key) {
     if (key.code === 'Space') keyPressed = true;
+    // Change camera perspective to 2D
+    else if (key.code === 'KeyP') {
+        twoDimensional = true;
+        camera.rotation.set(0, Math.PI / 2, 0);
+        camera.position.set(40, 0, -20);
+    }
 }
 
 function onKeyUp(key) {
@@ -139,28 +157,30 @@ function onKeyUp(key) {
 }
 
 // Start the game with a click
-document.addEventListener('click', () => {
+function onClick() {
     document.body.requestPointerLock();
     player.canMove = true;
     for (const pillar of pillars) {
         pillar.canMove = true;
     }
-});
+}
 
 // Looking around in first person
-document.addEventListener('mousemove', event => {
-    if (!document.pointerLockElement) return;
+function onMouseMove(event) {
+    if (!document.pointerLockElement || twoDimensional) return;
 
-    yaw -= event.movementX * rotationSpeed;
-    pitch -= event.movementY * rotationSpeed;
+    yaw -= event.movementX * viewSpeed;
+    pitch -= event.movementY * viewSpeed;
 
-    yaw = THREE.MathUtils.clamp(yaw, minYaw, maxYaw);
-    pitch = THREE.MathUtils.clamp(pitch, -Math.PI / 2, Math.PI / 2);
+    yaw = THREE.MathUtils.clamp(yaw, minView, maxView);
+    pitch = THREE.MathUtils.clamp(pitch, minView, maxView);
 
     camera.rotation.y = yaw;
     camera.rotation.x = pitch;
-});
+}
 
+document.addEventListener('click', onClick);
+document.addEventListener('mousemove', onMouseMove);
 document.addEventListener('keydown', onKeyDown);
 document.addEventListener('keyup', onKeyUp);
 
